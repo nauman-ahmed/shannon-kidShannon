5e44ac74af3387d84430383a798d533ce545d835
Nauman Ahmed <35535536+nauman-ahmed@users.noreply.github.com>
2023-10-10 18:46:54 +0200

    changed priority

--- src/containers/kidShanon/searchPages/searchByArtist.js
+++ src/containers/kidShanon/searchPages/searchByArtist.js
@@ -396,7 +396,7 @@ function SearchByArtist(props) {
                                   loading="lazy"
                                   role="presentation"
                                   decoding= "async"
-                                  fetchpriority= {keys % 2 === 0 ? "high" : "low"}
+                                  fetchpriority= {keys % 2 === 0 ? "high" : "medium"}
                                 ></img>
                               </div>
                             ))
